1a8f16d2058973f327c20b3216814ad939c9674f
SynergyShadow <36507966+shadowtigercool@users.noreply.github.com>
2023-12-04 07:01:03 -0500

    Update CustomPlayerSkinEntity.java

--- src/main/java/fzmm/zailer/me/client/renderer/customSkin/CustomPlayerSkinEntity.java
+++ src/main/java/fzmm/zailer/me/client/renderer/customSkin/CustomPlayerSkinEntity.java
@@ -42,9 +42,11 @@ public class CustomPlayerSkinEntity extends OtherClientPlayerEntity implements I
     }
 
     /**
+     * mods like DinnerMod may try to query the entity name and if the entity does not have a
      * name it returns null, which may result in a crash if the other mod does not check for null.
      */
     public String getEntityName() {
         return this.getGameProfile().getName();
     }
 }
+
