d4c2bd4707f26a66e82fcd786a2cf2d798d1d6af
SynergyShadow <36507966+shadowtigercool@users.noreply.github.com>
2023-12-04 07:11:32 -0500

    Update CustomPlayerSkinEntity.java

--- src/main/java/fzmm/zailer/me/client/renderer/customSkin/CustomPlayerSkinEntity.java
+++ src/main/java/fzmm/zailer/me/client/renderer/customSkin/CustomPlayerSkinEntity.java
@@ -1,11 +1,8 @@
 package fzmm.zailer.me.client.renderer.customSkin;
 
-package fzmm.zailer.me.client.renderer.customSkin;
-
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.util.Identifier;
 
 import java.util.UUID;
@@ -18,14 +15,9 @@ public class CustomPlayerSkinEntity extends OtherClientPlayerEntity implements I
     private Identifier skin;
     private String model;
 
-    private TextFieldWidget seedField; // Assuming seedField is a TextFieldWidget
-
     public CustomPlayerSkinEntity(ClientWorld world) {
         super(world, new GameProfile(UUID.randomUUID(), null));
         this.getDataTracker().set(PLAYER_MODEL_PARTS, Byte.MAX_VALUE);
-
-        // Initialize seedField if necessary
-        this.seedField = new TextFieldWidget(/* parameters */);
     }
 
     @Override
@@ -55,4 +47,5 @@ public class CustomPlayerSkinEntity extends OtherClientPlayerEntity implements I
      */
     public String getEntityName() {
         return this.getGameProfile().getName();
-    
+    }
+}
